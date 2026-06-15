15bcafb3a54b1194aa7a6203a98635fd8b20c41c
Ranak Shah <110567117+thorlaidanegg@users.noreply.github.com>
2026-06-15 18:07:45 +0530

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.ts
@@ -0,0 +1,33 @@
+
+// class ListNode {
+//     val
+//     next
+//     constructor(val = 0, next = null) {
+//         this.val = val;
+//         this.next = next;
+//     }
+// }
+ 
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head: ListNode | null): ListNode {
+
+        let curr = head    
+        let prev = null
+
+        if(curr === null) return null
+
+        while(curr != null){
+            let next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        }
+
+        return prev
+    }
+}
